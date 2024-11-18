df70b0252be35d36bf9a3942fed986c6132922b8
jeffellenbogen <jeffellenbogen@users.noreply.github.com>
2024-11-18 13:28:22 -0700

    Update neoPixelPlus.py
    added rotate function

--- neoPixelPlus.py
+++ neoPixelPlus.py
@@ -5,3 +5,12 @@ class neoPixelPlus(neopixel.NeoPixel)
 	def __init__(self, pin, numPixels):
 		super().__init__(pin,numPixel)
 		self.numPixels = numPixels
+
+	def rotate(strip, direction):
+		length = self.numPixels
+		# Rotate pixels left
+		if (direction == -1):
+			tempPixelData = strip[0]
+			for index in range(length-1):
+				strip[index]=strip[index+1]
+			strip[length-1]=tempPixelData			
